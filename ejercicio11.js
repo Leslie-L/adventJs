1900a9a74e84df66662c60094fb16fd9b5f04b21
Leslie-L <leslie.centeno@galileo.edu>
2023-04-12 12:53:03 -0600

    el ejercicio 10 necesita mejor

---
+++ ejercicio11.js
@@ -0,0 +1,13 @@
+function getCompleted(part, total) {
+    const timePart = part.split(':');
+    const timeTotal
+    return '1/1'
+
+}
+
+console.log(getCompleted('01:00:00', '03:00:00')) // '1/3'
+//console.log(getCompleted('02:00:00', '04:00:00')) // '1/2'
+//console.log(getCompleted('01:00:00', '01:00:00')) // '1/1'
+//console.log(getCompleted('00:10:00', '01:00:00')) // '1/6'
+//console.log(getCompleted('01:10:10', '03:30:30')) // '1/3'
+//console.log(getCompleted('03:30:30', '05:50:50')) // '3/5
